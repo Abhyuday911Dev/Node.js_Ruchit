a7b3a0e1143107982b14416997ea8dde10daafb4
Abhyuday <abhyuday911@gmail.com>
2023-07-06 16:11:50 +0530

    sasta data transfer

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,21 @@ router.get("/", function (req, res, next) {
   res.render("homepage", { title: "Ruchit", age: "22", gf: "null" });
 });
 
-router.get("/", function (req, res, next) {
-  res.render("homepage", { title: "Ruchit", age: "22", gf: "null" });
+router.get("/login", function (req, res, next) {
+  res.render("login");
+});
+
+router.post("/login", function (req, res, next) {
+  // console.log(req.body.name);
+  // console.log(req.body.age);
+
+
+  // dtabase me kuch checking hui fir user mila ya nahi mila
+  user = true;
+  // dtabase me kuch checking hui fir user mila ya nahi mila
+
+  if (!user) res.redirect("/login");
+  res.render("profile", { name: req.body.name, age: req.body.age });
 });
 
 module.exports = router;
