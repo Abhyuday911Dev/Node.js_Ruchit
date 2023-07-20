5de32857b138bc151151aac82c323d09ca0cfe4d
Abhyuday <abhyuday911@gmail.com>
2023-07-20 23:57:08 +0530

    user model

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,37 @@
 var express = require("express");
 var router = express.Router();
+const User = require("./userModel");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("homepage", { title: "Ruchit", age: "22", gf: "null" });
 });
 
-router.get("/login", function (req, res, next) {
+/* GET register page. */
+router.get("/register", function (req, res, next) {
+  res.render("register");
+});
+
+/* post register user. */
+router.post("/register", async function (req, res, next) {
+  // console.log(req.body.name);
+  // console.log(req.body.age);
+
+  const createUser = await new User(req.body).save();
+
+  res.json(createUser);
+});
+
+router.get("/login", async function (req, res, next) {
   res.render("login");
+
+  // const foundUser=  await User.find({name:"ruchit"})
+  // res
 });
 
 router.post("/login", function (req, res, next) {
   // console.log(req.body.name);
   // console.log(req.body.age);
-
 
   // dtabase me kuch checking hui fir user mila ya nahi mila
   user = true;
